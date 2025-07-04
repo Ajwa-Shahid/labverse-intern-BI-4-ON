4c8f0b826a52a96414f959432f22c16fcefa4139
Ajwa-Shahid <ajwashahid597@gmail.com>
2025-07-04 18:47:50 +0500

    Delete Internee_Tasks directory

--- Internee_Tasks/Day 1_11/8Concatenation.js
+++
@@ -1,6 +0,0 @@
-//Concatenation
-let str_ing1= "12";
-let str_ing2= "12";
-let final_String= +str_ing1 +  +str_ing2;  //+before strings will make it Number 
-console.log(final_String);      //24
-console.log(typeof final_String);  //number
